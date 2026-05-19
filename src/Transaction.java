6de24817d4587d7c2c3c6c2a4736ae3b4d8bdab8
soundarya966 <84025021+soundarya966@users.noreply.github.com>
2026-05-19 21:42:59 +0530

    Update Transaction.java

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,1 +1,20 @@
+public class Transaction {
 
+    private String sender;
+    private String receiver;
+    private double amount;
+
+    public Transaction(String sender, String receiver, double amount) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.amount = amount;
+    }
+
+    public void displayTransaction() {
+        System.out.println("---------------------------------");
+        System.out.println("Sender: " + sender);
+        System.out.println("Receiver: " + receiver);
+        System.out.println("Amount: " + amount);
+        System.out.println("---------------------------------");
+    }
+}
