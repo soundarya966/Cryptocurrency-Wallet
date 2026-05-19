9fba5ba6303aed265e4d3bd3a949963185d2d908
soundarya966 <84025021+soundarya966@users.noreply.github.com>
2026-05-19 21:42:28 +0530

    Update Wallet.java

--- src/Wallet.java
+++ src/Wallet.java
@@ -1,1 +1,32 @@
+public class Wallet {
 
+    private String walletAddress;
+    private String ownerName;
+    private double balance;
+
+    public Wallet(String ownerName) {
+        this.ownerName = ownerName;
+        this.walletAddress = StringUtil.applySha256(ownerName + System.currentTimeMillis()).substring(0, 12);
+        this.balance = 1000.0;
+    }
+
+    public String getWalletAddress() {
+        return walletAddress;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void addBalance(double amount) {
+        balance += amount;
+    }
+
+    public void deductBalance(double amount) {
+        balance -= amount;
+    }
+}
